96f61197864a22078fd8431eee899b45b24df6b6
r6v4 <r6v4@pm.me>
2024-09-03 23:41:42 +0800

    Update package.lisp

--- package.lisp
+++ package.lisp
@@ -3,16 +3,12 @@
 (defpackage #:cl-linux-queue
     (:use :cl :cl-user :cffi)
     (:export 
-        :string-to-octets ;
-        :octets-to-string ;
+        :string-to-vector
+        :vector-to-string 
         :string-message-to-list
-        :octets-message-to-list
+        :vector-message-to-list
         :list-to-string-message
-        :list-to-octets-message
-        :string-message-to-list-function-maker
-        :octets-message-to-list-function-maker
-        :list-to-string-message-function-maker
-        :list-to-octets-message-function-maker
-        :find-from-message-list ;
-        :add-to-message-list ;
+        :list-to-vector-message
+        :find-from-message-list 
+        :add-to-message-list 
         ))
